d300b546832c3dae57b8dc6183bf85c978b6402e
porsak <jonnson94@gmail.com>
2021-01-05 00:42:32 +0100

    V1.3

--- app/src/main/java/com/example/viruscheckapp/SearchActivity.kt
+++ app/src/main/java/com/example/viruscheckapp/SearchActivity.kt
@@ -3,6 +3,7 @@ package com.example.viruscheckapp
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
@@ -17,44 +18,35 @@ private val LOG_TAG = SearchActivity::class.java.simpleName
 private var HashSum = ""
 private var APIKey = ""
 
+private var ResponseText = ""
+
+private const val KEY_RESULT = "response"
+private const val KEY_HASH = "hash"
+private const val KEY_APIKEY = "apikey"
+
 class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-    }
 
-    override fun onStart() {
-        super.onStart()
         Log.d(LOG_TAG, "-------")
-        Log.d(LOG_TAG, "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(LOG_TAG, "-------")
-        Log.d(LOG_TAG, "onResume")
-    }
-
-    fun searchFile(view: View) {
-        val intent = Intent()
-            .setType("*/*")
-            .setAction(Intent.ACTION_GET_CONTENT)
-
-        startActivityForResult(Intent.createChooser(intent, "Select a file"), 111)
-    }
-/*
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == 111 && resultCode == RESULT_OK) {
-            val selectedFile = data?.data //The uri with the location of the file
-            val resultText: TextView = findViewById(R.id.textView2)
-            resultText.text = selectedFile.toString()
-            DataPath = selectedFile.toString()
+        Log.d(LOG_TAG, "onCreate")
+        if (savedInstanceState != null) {
+            ResponseText = savedInstanceState.getString(KEY_RESULT).toString()
+            HashSum = savedInstanceState.getString(KEY_HASH).toString()
+            APIKey = savedInstanceState.getString(KEY_APIKEY).toString()
         }
+
+        var resultHash: TextView = findViewById(R.id.textView5)
+        resultHash.text = ResponseText
+
+        val text: TextView = findViewById<EditText>(R.id.editTextTextPersonName)
+        text.text = HashSum
+        val text2: TextView = findViewById<EditText>(R.id.editTextTextPersonName2)
+        text2.text = APIKey
+
     }
-*/
 
     fun runAPISearch(view: View) {
         Log.d(LOG_TAG, "-------");
@@ -78,11 +70,64 @@ class SearchActivity : AppCompatActivity() {
                 val (bytes, error) = result
                 if (bytes != null) {
                     println("[response bytes] ${String(bytes)}")
+
+                    ResponseText = response.toString()
+                    Log.d(LOG_TAG, "-------")
+                    Log.d(LOG_TAG, "Response control")
                     var resultHash: TextView = findViewById(R.id.textView5)
-                    resultHash.text = response.toString()
+                    resultHash.text = ResponseText
                 }
             }
     }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        if (savedInstanceState != null) {
+            super.onSaveInstanceState(savedInstanceState)
+        }
+        Log.d(LOG_TAG, "-------")
+        Log.d(LOG_TAG, "onSave")
+        savedInstanceState.putString(KEY_RESULT, ResponseText)
+        savedInstanceState.putString(KEY_HASH, HashSum)
+        savedInstanceState.putString(KEY_APIKEY, APIKey)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(LOG_TAG, "-------")
+        Log.d(LOG_TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(LOG_TAG, "-------")
+        Log.d(LOG_TAG, "onStop")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(LOG_TAG, "-------")
+        Log.d(LOG_TAG, "onResume")
+    }
+
+    fun searchFile(view: View) {
+        val intent = Intent()
+            .setType("*/*")
+    .setAction(Intent.ACTION_GET_CONTENT)
+
+    startActivityForResult(Intent.createChooser(intent, "Select a file"), 111)
+    }
+ /*
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 111 && resultCode == RESULT_OK) {
+            val selectedFile = data?.data //The uri with the location of the file
+            val resultText: TextView = findViewById(R.id.textView2)
+            resultText.text = selectedFile.toString()
+            DataPath = selectedFile.toString()
+        }
+    }
+*/
 
 /*
     private fun sendPost() {
@@ -102,9 +147,4 @@ class SearchActivity : AppCompatActivity() {
     }
 */
 
-    override fun onPause() {
-        super.onPause()
-        Log.d(LOG_TAG, "-------")
-        Log.d(LOG_TAG, "onPause")
-    }
 }
