37cc457f3ea0292ecfc9e99dbe49878a9198ae48
porsak <jonnson94@gmail.com>
2021-01-07 18:17:22 +0100

    V1.4

--- app/src/main/java/com/example/viruscheckapp/InvestigateActivity.kt
+++ app/src/main/java/com/example/viruscheckapp/InvestigateActivity.kt
@@ -2,10 +2,46 @@ package com.example.viruscheckapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.io.InputStream
+import java.io.OutputStream
+
+private val LOG_TAG = SearchActivity::class.java.simpleName
 
 class InvestigateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_investigate)
+    }
+    /*
+    object UserPreferencesSerializer : Serializer<giveData> {
+        override val defaultValue: UserPreferences = giveData.getDefaultInstance()
+        override fun readFrom(input: InputStream): giveData {
+            try {
+                return giveData.parseFrom(input)
+            } catch (exception: InvalidProtocolBufferException) {
+                throw CorruptionException("Cannot read proto.", exception)
+            }
+        }
+
+        override fun writeTo(t: UserPreferences, output: OutputStream) = t.writeTo(output)
+    }*/
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(LOG_TAG, "-------")
+        Log.d(LOG_TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(LOG_TAG, "-------")
+        Log.d(LOG_TAG, "onStop")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(LOG_TAG, "-------")
+        Log.d(LOG_TAG, "onResume")
     }
 }
--- app/src/main/java/com/example/viruscheckapp/SearchActivity.kt
+++ app/src/main/java/com/example/viruscheckapp/SearchActivity.kt
@@ -1,18 +1,17 @@
 package com.example.viruscheckapp
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.github.kittinunf.fuel.Fuel
+import org.json.JSONObject
+import java.io.BufferedWriter
 import java.io.File
-import java.io.FileInputStream
-import java.io.IOException
+import java.io.FileWriter
+
 
 private val LOG_TAG = SearchActivity::class.java.simpleName
 private var HashSum = ""
@@ -63,23 +62,35 @@ class SearchActivity : AppCompatActivity() {
 
     private fun sendGet(){
         Fuel.get("https://www.virustotal.com/vtapi/v2/file/report?apikey=$APIKey&resource=$HashSum")
-            .response {
-                    request, response, result -> println(request)
+            .response { request, response, result -> println(request)
                 println(response)
+
+                Log.d(LOG_TAG, "1-------")
                 Log.d(LOG_TAG, response.toString())
+
+
                 val (bytes, error) = result
                 if (bytes != null) {
+                    Log.d(LOG_TAG, "2-------")
                     println("[response bytes] ${String(bytes)}")
-
-                    ResponseText = response.toString()
-                    Log.d(LOG_TAG, "-------")
-                    Log.d(LOG_TAG, "Response control")
-                    var resultHash: TextView = findViewById(R.id.textView5)
-                    resultHash.text = ResponseText
                 }
+
+                Log.d(LOG_TAG, "-------")
+                Log.d(LOG_TAG, "Response control")
+                ResponseText = response.toString()
+
+                var ResultHash: TextView = findViewById(R.id.textView5)
+                ResultHash.text = ResponseText
+
             }
     }
 
+    private fun saveToFile() {
+        var formatResponseText = ResponseText
+
+        formatResponseText.toCharArray()
+        val file: File = formatResponseText
+    }
     override fun onSaveInstanceState(savedInstanceState: Bundle) {
         if (savedInstanceState != null) {
             super.onSaveInstanceState(savedInstanceState)
@@ -89,6 +100,13 @@ class SearchActivity : AppCompatActivity() {
         savedInstanceState.putString(KEY_RESULT, ResponseText)
         savedInstanceState.putString(KEY_HASH, HashSum)
         savedInstanceState.putString(KEY_APIKEY, APIKey)
+    }
+
+    fun runSaveToFile(view: View) {
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "Button clicked!")
+
+
     }
 
     override fun onPause() {
@@ -109,13 +127,6 @@ class SearchActivity : AppCompatActivity() {
         Log.d(LOG_TAG, "onResume")
     }
 
-    fun searchFile(view: View) {
-        val intent = Intent()
-            .setType("*/*")
-    .setAction(Intent.ACTION_GET_CONTENT)
-
-    startActivityForResult(Intent.createChooser(intent, "Select a file"), 111)
-    }
  /*
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
